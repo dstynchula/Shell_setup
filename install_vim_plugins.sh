7771236246823ce86eb190708063666db7047fa2
Dan Stynchula <dstynchula@users.noreply.github.com>
2019-07-25 11:19:39 -0700

    sets up vim with developer plugins for code completion

---
+++ install_vim_plugins.sh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+
+# Install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+#add Pathogen to ~/.vimrc
+cat <<EOT>> ~/.vimrc
+execute pathogen#infect()
+set mouse=a
+set number
+syntax on
+filetype plugin indent on
+EOT
+
+# Install vim-sensible
+cd ~/.vim/bundle && \
+git clone https://github.com/tpope/vim-sensible.git
+
+# Install vim-go
+git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+
+# Install NERD Tree
+cd ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree
+
+# Make sure nerdtree opens up if Vim is opened up with no file specified.
+cat <<EOT>> ~/.vimrc
+autocmd StdinReadPre * let s:std_in=1
+autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif
+EOT
+
+# Lets not forget youcompleteme
+cd ~/.vim/bundle
+git clone https://github.com/valloric/youcompleteme
